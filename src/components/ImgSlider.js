3b5df5aa7ae054f7706d346edf4d47610f463b67
GideonOdiokine <gidysmartodiokineh>
2021-07-09 02:32:41 +0100

    added images to slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -15,15 +15,26 @@ const ImgSlider = () => {
   };
   return (
     <Carousel {...settings}>
-      <div>
-        <h2>1</h2>
-      </div>
-      <div>
-        <h2>2</h2>
-      </div>
-      <div>
-        <h2>3</h2>
-      </div>
+      <Wrap>
+        <a>
+          <img src="images/slider-badging.jpg"></img>
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src="images/slider-scale.jpg"></img>
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src="images/slider-badag.jpg"></img>
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src="images/slider-scales.jpg"></img>
+        </a>
+      </Wrap>
     </Carousel>
   );
 };
@@ -32,6 +43,48 @@ const Carousel = styled(Slider)`
   margin-top: 20px;
   & > button {
     opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+    &:hover {
+      opacity: 1;
+        transition opacity 0.2s ease 0s;
+    }
+  }
+  ul li button{
+      &:before{
+          font-size:10px;
+          color:rbg(150, 158, 171);
+      }
+  }
+  li.slick-active button:before{
+      color:white;
+  }
+  .slick-list{
+      overflow:initial
+  }
+  .slick-prev{
+      left:-75px;
+  }
+  .slick-next{
+    right:-75px;
+}
+`;
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+  a {
+    position: relative;
+    padding: 4px;
+    display: block;
+    cursor: pointer;
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 /69%) 0px 26px 30px rgb(0 0 0/ 73%);
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 `;
 export default ImgSlider;
