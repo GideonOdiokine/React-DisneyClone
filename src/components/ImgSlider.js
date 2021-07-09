f1316e52cc51162bcda37dcb85d2f4525e054232
GideonOdiokine <gidysmartodiokineh>
2021-07-09 01:47:17 +0100

    added mySlider component

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ImgSlider = () => {
-    return (
-        <div>
-            Image Slider
-        </div>
-    )
-}
+  let settings = {
+    dots: true,
+    Infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...settings}>
+      <div>
+        <h2>1</h2>
+      </div>
+      <div>
+        <h2>2</h2>
+      </div>
+      <div>
+        <h2>3</h2>
+      </div>
+    </Carousel>
+  );
+};
 
-export default ImgSlider
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+  & > button {
+    opacity: 0;
+  }
+`;
+export default ImgSlider;
